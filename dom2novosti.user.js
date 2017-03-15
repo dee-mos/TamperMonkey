c3fcca5117a0fbab39cf769e9e3b629e29d317e0
dee-mos <dee-ru@yandex.ru>
2017-03-15 14:18:40 +0300

    refactoring

--- dom2novosti.user.js
+++ dom2novosti.user.js
@@ -1,6 +1,6 @@
-// ==UserScript==
+Ôªø// ==UserScript==
 // @name         dom2novosti
-// @namespace    DeeTamper
+// @namespace    dee-tampermonkey-scripts
 // @version      0.1
 // @description  dom2novosti
 // @author       Dee
@@ -10,14 +10,17 @@
 // @require      file://E:\DimusGreasemonkey\libs\dom.js
 // @require      file://E:\DimusGreasemonkey\libs\KolorWheel.min.js
 // @require      file://E:\DimusGreasemonkey\3rd\tsl.js
-// @resource     animated_css  file://E:\DimusGreasemonkey\css\animated.css
-// @resource     animation2_css  file://E:\DimusGreasemonkey\css\animation2.css
+// @resource     animated_css    https://github.com/dee-mos/TamperMonkey/css/animated.css
+// @resource     animation2_css  https://github.com/dee-mos/TamperMonkey/css/animation2.css
 // @grant        GM_addStyle
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @grant        GM_getResourceURL
 // @grant        GM_getResourceText
 // ==/UserScript==
+
+
+
 
 //debugger;
 
@@ -78,128 +81,127 @@ function get_articles(response)
 
 function process_page()
 {
-$("body").css("cssText", "background-image: none !important;");
+		$("body").css("cssText", "background-image: none !important;");
 
-//============================================================================================================================================    
+		//<link rel="shortcut icon" href="http://dom2novosti.ru/wp-content/uploads/2016/11/favicon.ico" title="Favicon" /><!--[if IE]>    
+		$('link[title="Favicon"]').attr("href","http://www.stackoverflow.com/favicon.ico");
+				
+		$("#categories-4").remove();
+		$("#recent-posts-2").remove();
+		$("div.logo").remove();
+		$("#theme-header > div").remove();
+		$("footer").remove();
+		$("div.footer-bottom").remove();
 
-//<link rel="shortcut icon" href="http://dom2novosti.ru/wp-content/uploads/2016/11/favicon.ico" title="Favicon" /><!--[if IE]>    
-$('link[title="Favicon"]').attr("href","http://www.stackoverflow.com/favicon.ico");
-    
-$("#categories-4").remove();
-$("#recent-posts-2").remove();
-$("div.logo").remove();
-$("#theme-header > div").remove();
-$("footer").remove();
-$("div.footer-bottom").remove();
+		$(".content").width("800");
 
-$(".content").width("800");
+		$("#main-nav").css("cssText", "background-color: black !important;");
 
-$("#main-nav").css("cssText", "background-color: black !important;");
+		$("span.comment-author-link.cwp-author-link").filter( function(index) { return "KPOBOCOC" == $(this).text(); } ).css( "color", "red" );
 
-$("span.comment-author-link.cwp-author-link").filter( function(index) { return "KPOBOCOC" == $(this).text(); } ).css( "color", "red" );
+		$("img.attachment-tie-large.size-tie-large.wp-post-image").width(100);
 
-$("img.attachment-tie-large.size-tie-large.wp-post-image").width(100);
+		$('article a.more-link').hide();
+				
+		// hide text of article    
+		$('article div.entry').hide();
 
-$('article a.more-link').hide();
-    
-// hide text of article    
-$('article div.entry').hide();
+		hdr = $('#theme-header');
+				
+		$('<input />', { type: 'checkbox', id: 'show_hide_entry', class: 'trigger_show_entry' }).appendTo(hdr);  
+		$('<label />', { text: 'Show/Hide' }).appendTo(hdr);    
+		$('#show_hide_entry').click(function() { $('article div.entry').toggle(); });     
+				
+		$("a.more-link").each(function(index)
+		{
+			//console.log($(this).prop('href'));
+				last_span = $(this).closest('article').find('span:last');
+			new_span = last_span.clone().addClass('new_messages_counter').text('');
+			last_span.after(new_span);
 
-hdr = $('#theme-header');
-    
-$('<input />', { type: 'checkbox', id: 'show_hide_entry', class: 'trigger_show_entry' }).appendTo(hdr);  
-$('<label />', { text: 'Show/Hide' }).appendTo(hdr);    
-$('#show_hide_entry').click(function() { $('article div.entry').toggle(); });     
-    
-$("a.more-link").each(function(index)
-{
-	//console.log($(this).prop('href'));
-    last_span = $(this).closest('article').find('span:last');
-	new_span = last_span.clone().addClass('new_messages_counter').text('');
-	last_span.after(new_span);
+				$.ajax({
+					url: $(this).prop('href'),
+				counter_elem: new_span,
+					success: function( data )
+				{
+				new_count = 0;
+					if( data.match(/<link rel='shortlink' href='http:\/\/dom2novosti.ru\/\?p=(\d+)' \/>/gi) )
+				{
+					post_id = RegExp.$1;
+					if( data.match(/(<div id="comments">[\s\S]*?)<!-- #comments -->/gim) )
+					{
+									comments = $('li', $(RegExp.$1));
+									comments.each(set_comment_attributes);
+						last_msg = dt.str2datetime( GM_getValue('postid-'+post_id, null) );
+						comments.each(function(index)
+						{
+							datetime = dt.str2datetime( $(this).attr('timestamp') );
+							if(datetime > last_msg) new_count++;
+						});
+					}
+					if(new_count > 0) { this.counter_elem.text('–ù–æ–≤—ã—Ö ' + new_count); } else { this.counter_elem.hide(); }
+				}
+			}
+			});
 
-    $.ajax({
-      url: $(this).prop('href'),
-	  counter_elem: new_span,
-      success: function( data )
-    {
-	  new_count = 0;
-      if( data.match(/<link rel='shortlink' href='http:\/\/dom2novosti.ru\/\?p=(\d+)' \/>/gi) )
-	  {
-		  post_id = RegExp.$1;
-		  if( data.match(/(<div id="comments">[\s\S]*?)<!-- #comments -->/gim) )
-		  {
-              comments = $('li', $(RegExp.$1));
-              comments.each(set_comment_attributes);
-			  last_msg = dt.str2datetime( GM_getValue('postid-'+post_id, null) );
-			  comments.each(function(index)
-			  {
-				  datetime = dt.str2datetime( $(this).attr('timestamp') );
-				  if(datetime > last_msg) new_count++;
-			  });
-		  }
-		  if(new_count > 0) { this.counter_elem.text('ÕÓ‚˚ı ' + new_count); } else { this.counter_elem.hide(); }
-	  }
-	}
-  });
+		});
 
-});
+		// insert checkbox to hide/show articles    
+		hdr = $('#theme-header');
+		$('<input />', { type: 'checkbox', id: 'show_hide_articles', value: name }).appendTo(hdr);  
+		$('<label />', { 'for': 'show_hide_articles', text: 'Show/Hide articles' }).appendTo(hdr);    
+				
+		post_id = get_post_id();
 
-// insert checkbox to hide/show articles    
-hdr = $('#theme-header');
-$('<input />', { type: 'checkbox', id: 'show_hide_articles', value: name }).appendTo(hdr);  
-$('<label />', { 'for': 'show_hide_articles', text: 'Show/Hide articles' }).appendTo(hdr);    
-    
-post_id = get_post_id();
+		//console.log("post_id = " + post_id );
 
-//console.log("post_id = " + post_id );
+		last_msg = dt.str2datetime( GM_getValue(post_id, null) ) ;
 
-last_msg = dt.str2datetime( GM_getValue(post_id, null) ) ;
+		maxDate = new Date(1500,1,1);
 
-maxDate = new Date(1500,1,1);
+		var gradient = make_gradient('#2FD9FB', '#FAD1D1', 10);
 
-var gradient = make_gradient('#2FD9FB', '#FAD1D1', 10);
+		comments = $('div#comments li');
+		comments.each(set_comment_attributes);
 
-comments = $('div#comments li');
-comments.each(set_comment_attributes);
+		$("cite").each(function( index )
+		{
+				cur_msg_datetime = dt.str2datetime($(this).next().text());
+				diff = (Date.now() - cur_msg_datetime) / (1000 * 60 * 60); // in hours
+				diffh = Math.trunc(diff);
 
-$("cite").each(function( index )
-{
-    cur_msg_datetime = dt.str2datetime($(this).next().text());
-    diff = (Date.now() - cur_msg_datetime) / (1000 * 60 * 60); // in hours
-    diffh = Math.trunc(diff);
+				msg_date = add_date_time( $(this).next().text() );
 
-    msg_date = add_date_time( $(this).next().text() );
+				elem_li = $(this).closest( "li" );
+				elem_ava = elem_li.find('div.comment-avatar');
 
-    elem_li = $(this).closest( "li" );
-    elem_ava = elem_li.find('div.comment-avatar');
+			elem_ava.after(	$("<div class='dot'></div>") );
 
-	elem_ava.after(	$("<div class='dot'></div>") );
+				if(diffh >= 0 && diffh < gradient.length) {  $(this).css({'font-weight':'bold'}).closest( "li" ).find( "*" ).css( "background-color", gradient[diffh] );  }
 
-    if(diffh >= 0 && diffh < gradient.length) {  $(this).css({'font-weight':'bold'}).closest( "li" ).find( "*" ).css( "background-color", gradient[diffh] );  }
+				if("KPOBOCOC" == $(this).text())
+				{
+					$(this).css({'color' : 'red', 'font-weight':'bold'}).closest( "li" ).find( "*" ).css( "background-color", "#b9ffd5" );
+				}
 
-    if("KPOBOCOC" == $(this).text())
-    {
-      $(this).css({'color' : 'red', 'font-weight':'bold'}).closest( "li" ).find( "*" ).css( "background-color", "#b9ffd5" );
-    }
+				if(msg_date > last_msg)  { elem_ava.css( "background-color", "#fdff8d"); elem_ava.addClass('animation_01'); }
+		});
 
-    if(msg_date > last_msg)  { elem_ava.css( "background-color", "#fdff8d"); elem_ava.addClass('animation_01'); }
-});
+		/**********
+		my_messages = $("cite").filter( function(index) { return "KPOBOCOC" == $(this).text(); } );
+		my_messages.css({'color' : 'red', 'font-weight':'bold'}).closest( "li" ).find( "*" ).css( "background-color", "#b9ffd5" );
 
-/**********
-my_messages = $("cite").filter( function(index) { return "KPOBOCOC" == $(this).text(); } );
-my_messages.css({'color' : 'red', 'font-weight':'bold'}).closest( "li" ).find( "*" ).css( "background-color", "#b9ffd5" );
+		new_messages = $("cite").filter( function(index) { msg_date = add_date_time( $(this).next().text() ); return msg_date > last_msg; } );
+		new_messages.css({'color' : 'red', 'font-weight':'bold'}).closest( "li" ).find( "*" ).css( "background-color", "#fdff8d" );
+		***********/
 
-new_messages = $("cite").filter( function(index) { msg_date = add_date_time( $(this).next().text() ); return msg_date > last_msg; } );
-new_messages.css({'color' : 'red', 'font-weight':'bold'}).closest( "li" ).find( "*" ).css( "background-color", "#fdff8d" );
-***********/
+		max_date_str = dt.strftime('%d.%m.%Y at %H:%M',maxDate);
 
-max_date_str = dt.strftime('%d.%m.%Y at %H:%M',maxDate);
+		GM_setValue(post_id, max_date_str);
 
-GM_setValue(post_id, max_date_str);
-
-$('body').show();
-}
+		$('body').show();
+		
+} // function process_page()
 
 //=======================================================================================================================================
 
@@ -220,7 +222,7 @@ GM_addStyle(".new_messages_counter { border-radius: 10px; background: #ff0000; p
 if(is_root_page)
 {
   $.when(
-      get_page('http://dom2novosti.ru/page/2/'),
+    get_page('http://dom2novosti.ru/page/2/'),
 	  get_page('http://dom2novosti.ru/page/3/'),
 	  get_page('http://dom2novosti.ru/page/4/'),
 	  get_page('http://dom2novosti.ru/page/5/')
