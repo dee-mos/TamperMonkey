260d92e70873db43daec66d12d0dde18190fba42
dee-mos <dee-ru@yandex.ru>
2019-03-30 00:06:41 +0300

    Create echo.msk.user.js

---
+++ echo.msk.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name          Echo Msk Extender
+// @description   Расширение функционала "Эхо Москвы"
+// @namespace     https://greasyfork.org/ru/scripts/21933-kinopoisk-extender
+// @author        Dimus
+// @version       1.0.0
+// @homepage      https://greasyfork.org/ru/scripts/21933-kinopoisk-extender
+// @require       https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
+// @include       https://echo.msk.ru/*
+// @grant         GM_xmlhttpRequest
+// @grant         GM_addStyle
+// ==/UserScript==
+
+// body > div.lVWzaB3xkL4_cNhE > div.header > div.header_top > div > div.userblock > div > div > div:nth-child(2) > a 
+
