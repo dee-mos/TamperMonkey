6e9ce87712919670031cd1294dd0c3703d535c0c
dee-mos <dee-ru@yandex.ru>
2024-06-11 15:39:33 +0300

    Create github_test.js

---
+++ github_test.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Test Github script update
+// @namespace    http://tampermonkey.net/
+// @version      2024-06-11
+// @description  try to take over the world!
+// @author       Dimus
+// @match        https://lichess.org/training/mix
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=lichess.org
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
